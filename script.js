3d1678c677e6103c9eacee2edd5da711bf8f0283
Gloria Xi <gloriaxi@192-168-1-104.tpgi.com.au>
2025-02-19 01:27:43 +1100

    updated landing page

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const backButton = document.getElementById("backButton");
+    if (backButton) {
+        backButton.addEventListener("click", function () {
+            window.location.href = "index.html";
+        });
+    }
+})
