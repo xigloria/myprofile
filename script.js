eec4c43de7b473e3cbb7d58041fb181cb6cd3535
Gloria Xi <gloriaxi@192-168-1-104.tpgi.com.au>
2025-02-19 02:30:48 +1100

    incorporating interactive map

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const startButton = document.getElementById("startButton");
     const backButton = document.getElementById("backButton");
-    if (backButton) {
-        backButton.addEventListener("click", function () {
-            window.location.href = "index.html";
-        });
+    const mapContainer = document.getElementById("map-container");
+    const mainContainer = document.querySelector(".container");
+
+    let mapLoaded = false;
+    mapContainer.style.display = "none";
+
+    startButton.addEventListener("click", function () {
+        mainContainer.style.display = "none";
+        mapContainer.style.display = "block";
+
+        if (!mapLoaded) {
+            initializeMap();
+            mapLoaded = true;
+        }
+    });
+
+    backButton.addEventListener("click", function () {
+        mapContainer.style.display = "none";
+        mainContainer.style.display = "block";
+    });
+
+    function initializeMap() {
+        const map = L.map("map").setView([-33.8688, 151.2093], 13);
+
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
     }
 })
+
+
