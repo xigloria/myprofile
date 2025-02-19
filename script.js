eadf952df96c080894d55e306c0eae2f85028aee
Gloria Xi <gloriaxi@Glorias-MacBook-Pro.local>
2025-02-19 11:53:23 +1100

    adding modal

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
     const startButton = document.getElementById("startButton");
     const backButton = document.getElementById("backButton");
     const mapContainer = document.getElementById("map-container");
     const mainContainer = document.querySelector(".container");
+    const modal = document.getElementById("mapModal");
+    const closeModal = document.getElementById("closeModal");
 
     let mapLoaded = false;
     mapContainer.style.display = "none";
 
-    startButton.addEventListener("click", function () {
+    startButton.addEventListener("click", () => {
         mainContainer.style.display = "none";
         mapContainer.style.display = "block";
 
@@ -15,14 +17,22 @@ document.addEventListener("DOMContentLoaded", function () {
             initializeMap();
             mapLoaded = true;
         }
+
+        modal.classList.remove("hidden");
     });
 
-    backButton.addEventListener("click", function () {
+    backButton.addEventListener("click", () => {
         mapContainer.style.display = "none";
         mainContainer.style.display = "block";
+
+        modal.classList.add("hidden")
     });
 
-    function initializeMap() {
+    closeModal.addEventListener("click", () => {
+        modal.classList.add("hidden")
+    })
+
+    const initializeMap = () => {
         const map = L.map("map").setView([-33.8688, 151.2093], 3);
 
         L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -30,12 +40,14 @@ document.addEventListener("DOMContentLoaded", function () {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
+        
         const customIcon = L.icon({
             iconUrl: "starred.png",
             iconSize: [32, 32],
             iconAnchor: [16, 32],
         })
-        let marker = L.marker([-33.8688, 151.2093], { icon: customIcon }).addTo(map);
+        var marker = L.marker([-33.8688, 151.2093], { icon: customIcon }).addTo(map);
+        marker.bindPopup("<h3>I was born here!<h3>");
     }
 
 })
